904c8d3dbeec983fa541127dd0d570dcd830f60d
Bandera <42476974+Bandera1@users.noreply.github.com>
2020-09-13 04:10:34 +0300

    Added schedule system

--- StudentAccountingProject/MediatR/Course/Commands/SubscribeToCourseCommand.cs
+++ StudentAccountingProject/MediatR/Course/Commands/SubscribeToCourseCommand.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using Hangfire;
+using Hangfire.Common;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using StudentAccountingProject.DB;
 using StudentAccountingProject.DB.Helpers;
@@ -8,6 +10,7 @@ using StudentAccountingProject.MediatR.Course.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -20,10 +23,15 @@ namespace StudentAccountingProject.MediatR.Course.Commands
         public class SubscribeToCourseCommandHandler : BaseMediator, IRequestHandler<SubscribeToCourseCommand, SubscribeCourseViewModel>
         {
             private readonly IMediator Mediator;
+            IBackgroundJobClient BackgroundJobClient;
 
-            public SubscribeToCourseCommandHandler(EFDbContext context, IMediator mediator) : base(context)
+            public SubscribeToCourseCommandHandler(
+                EFDbContext context, 
+                IMediator mediator,
+                IBackgroundJobClient backgroundJobClient) : base(context)
             {
                 Mediator = mediator;
+                BackgroundJobClient = backgroundJobClient;
             }
 
             public async Task<SubscribeCourseViewModel> Handle(SubscribeToCourseCommand request, CancellationToken cancellationToken)
@@ -38,7 +46,7 @@ namespace StudentAccountingProject.MediatR.Course.Commands
                     };
                 }
 
-                var student = Context.BaseProfiles.FirstOrDefault(x => x.Id == request.DTO.StudentId);
+                var student = Context.Users.FirstOrDefault(x => x.Id == request.DTO.StudentId);
                 if (student == null)
                 {
                     return new SubscribeCourseViewModel
@@ -47,11 +55,11 @@ namespace StudentAccountingProject.MediatR.Course.Commands
                         ErrorMessage = "Student does not exist"
                     };
                 }
-                if (!Context.Users.FirstOrDefault(x => x.Id == student.UserId).EmailConfirmed)
+                if (!student.EmailConfirmed)
                 {
                     await Mediator.Send(new SendConfirmEmailCommand
                     {
-                        DTO = new Account.DTO.SendConfirmEmailDTO { UserId = student.UserId }
+                        DTO = new Account.DTO.SendConfirmEmailDTO { UserId = student.Id }
                     });
                     return new SubscribeCourseViewModel
                     {
@@ -61,16 +69,159 @@ namespace StudentAccountingProject.MediatR.Course.Commands
 
                 }
 
-                //if (student.Courses == null) student.Courses = new List<DB.Entities.Course>();
-
                 Context.StudentsToCourses.Add(new DB.Entities.StudentToCourse
                 {
                     CourseId = course.Id,
                     StudentId = student.Id
                 });
-                Context.SaveChanges();             
+                Context.SaveChanges();
+
+                System.Action<string, string, int> act = async (courseName, email, days) => {
+                    await Mediator.Send(new SendNotificationEmailCommand
+                    {
+                        DTO = new SendNotificationEmailDTO
+                        {
+                            CourseName = courseName,
+                            UserEmail = email,
+                            DaysCount = days
+                        }
+                    });
+                };
+
+                if ((course.DateOfStart - DateTime.Now).Days >= 30)
+                {
+                    var days = (course.DateOfStart - DateTime.Now).Days - 30;
+
+                    BackgroundJobClient.Schedule(
+                    () => act.Invoke(course.Name, student.Email, 30),
+                    TimeSpan.FromDays(days));
+
+                    BackgroundJobClient.Schedule(
+                    () => act.Invoke(course.Name, student.Email, 7),
+                    TimeSpan.FromDays(days + 23));
+
+                    int hour = 0;
+                    if(course.DateOfStart.Hour > 8)
+                    {
+                        if(DateTime.Now.Hour > 8)
+                        {
+                            days += 6;
+                            hour = DateTime.Now.Hour - 8;
+                        } 
+                        else
+                        {
+                            days += 7;
+                            hour = 8 - DateTime.Now.Hour;
+                        }
+                    } 
+                    else
+                    {
+                        if (DateTime.Now.Hour > 8)
+                        {
+                            days += 5;
+                            hour = DateTime.Now.Hour - 8;
+                        }
+                        else
+                        {
+                            days += 6;
+                            hour = 8 - DateTime.Now.Hour;
+                        }
+                    }
+
+                    BackgroundJobClient.Schedule(
+                    () => act.Invoke(course.Name, student.Email, 7),
+                    new TimeSpan(days,hour,0,0));
+                }
+                else
+                {
+                    var days = (course.DateOfStart - DateTime.Now).Days;
+
+                    if(days > 7)
+                    {
+                        days -= 7;
+                        BackgroundJobClient.Schedule(
+                        () => act.Invoke(course.Name, student.Email, 7),
+                        TimeSpan.FromDays(days));
+
+                        int hour = 0;
+                        if (course.DateOfStart.Hour > 8)
+                        {
+                            if (DateTime.Now.Hour > 8)
+                            {
+                                days += 6;
+                                hour = DateTime.Now.Hour - 8;
+                            }
+                            else
+                            {
+                                days += 7;
+                                hour = 8 - DateTime.Now.Hour;
+                            }
+                        }
+                        else
+                        {
+                            if (DateTime.Now.Hour > 8)
+                            {
+                                days += 5;
+                                hour = DateTime.Now.Hour - 8;
+                            }
+                            else
+                            {
+                                days += 6;
+                                hour = 8 - DateTime.Now.Hour;
+                            }
+                        }
+
+                        BackgroundJobClient.Schedule(
+                        () => act.Invoke(course.Name, student.Email, 7),
+                        new TimeSpan(days, hour, 0, 0));
+                    } 
+                    else
+                    {
+                        int hour = 0;
+                        if (course.DateOfStart.Hour > 8)
+                        {
+                            if (DateTime.Now.Hour > 8)
+                            {
+                                if((course.DateOfStart - DateTime.Now).Days < 7)
+                                {
+                                    days = 6 + (course.DateOfStart - DateTime.Now).Days;
+                                }
+                                days += 6;
+                                hour = DateTime.Now.Hour - 8;
+                            }
+                            else
+                            {
+                                if ((course.DateOfStart - DateTime.Now).Days < 7)
+                                {
+                                    days = 7 + (course.DateOfStart - DateTime.Now).Days;
+                                }
+                                hour = 8 - DateTime.Now.Hour;
+                            }
+                        }
+                        else
+                        {
+                            if (DateTime.Now.Hour > 8)
+                            {
+                                days += 5;
+                                hour = DateTime.Now.Hour - 8;
+                            }
+                            else
+                            {
+                                days += 6;
+                                hour = 8 - DateTime.Now.Hour;
+                            }
+                        }
+
+                        BackgroundJobClient.Schedule(
+                        () => act.Invoke(course.Name, student.Email, 7),
+                        new TimeSpan(days, hour, 0, 0));
+                    }              
+                }
+                
+
+
                 return new SubscribeCourseViewModel { Status = true };
-            }
+            }          
         }
     }
 }
---
+++ StudentAccountingProject/MediatR/Course/DTO/SendNotificationEmailDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAccountingProject.MediatR.Course.DTO
+{
+    public class SendNotificationEmailDTO
+    {
+        public string UserEmail { get; set; }
+        public string CourseName { get; set; }
+        public int DaysCount { get; set; }
+    }
+}
